1ee7c1876ae598ef5b01f91bff5693815b37505f
fightorqin <fightorqin@gmail.com>
2018-11-14 12:42:07 +0800

    SSR Mobile version first push

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+
+<template>
+<PageLayout>
+    <template slot="pagecont1">
+      test
+    </template>
+</PageLayout>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+<style scoped>
+.tophere1 {
+  font-family: "MyriadPro Light", Helvetica, Arial, sans-serif !important;
+  font-size: 1.5rem !important;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
+
+
+
+
+
+
+
